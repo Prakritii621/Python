b33c13863a3e6e14f4aa042b23a65d8404b657d3
Prakriti Acharya <126312189+Prakritii621@users.noreply.github.com>
2024-08-12 15:14:39 +0545

    Create whileLoop.py

---
+++ whileLoop.py
@@ -0,0 +1,15 @@
+#while loop
+'''
+while condition:
+  statement
+'''
+'''
+while (condition_where_loop_will_run): 
+  if (stopping_condition)==True:
+    break
+'''
+#continuously take an input from the user until adn unless they give a number divisible by 5
+while 'true':
+  a=input('enter a number:')  
+  if int(a)%5==0:
+    break
