fd92a15736d8b34ca9d19173d4935643c6bbdd97
Prakriti Acharya <126312189+Prakritii621@users.noreply.github.com>
2024-08-12 15:15:07 +0545

    Create function.py

---
+++ function.py
@@ -0,0 +1,18 @@
+'''
+functions:
+  definition:define the fxn and what it will do
+  calling function:Invoke or call that specific function
+  function definition:
+    def function_name(parameter):
+      print(parameters)
+    calling of fucntion:
+      function_name()
+'''
+def even_number_checker(x):
+  if x%2==0:
+    print('even')
+  else:
+    print('odd')
+even_number_checker(10)
+even_number_checker(45)
+even_number_checker(0)
