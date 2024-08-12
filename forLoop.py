11467f137f476384584b1e3e43b72ac05e286045
Prakriti Acharya <126312189+Prakritii621@users.noreply.github.com>
2024-08-12 15:00:59 +0545

    Create forLoop.py

---
+++ forLoop.py
@@ -0,0 +1,18 @@
+'''loop and iterations:
+-repeat same task over and over again
+-can be used for various cases -asking queries in
+
+-for loop and while loop
+-for loop used when we know the starting point and we know the ending point of the loop
+-while loop used when we don't know the ending point
+'''
+for i in range(0,10):
+  print(i)
+for j in range(0,10,2):
+  print(j)
+for a in range(1,100,2):
+  print(a)
+#else
+for b in range(1,100):
+  if b%2!=0:
+    print(b)
